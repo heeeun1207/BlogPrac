18270230298e29cc063f95b9897e56e47ad61e23
jungheeeun <jungheeeun272@gmail.com>
2023-09-12 17:52:29 +0900

    AuthForm 컴포넌트 만들기

---
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+/**
+ *회원가입 또는 로그인 폼을 보여준다.
+ */
+
+const AuthFormBlok = styled.div``;
+
+const AuthForm = () => {
+  return (
+    <AuthFormBlok>
+      AuthForm
+    </AuthFormBlok>
+  );
+};
+
+export default AuthForm;
